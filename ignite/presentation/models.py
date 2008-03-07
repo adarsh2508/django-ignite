9c858cc84468878151f78f5ef176d75a9b7c4bec
Rob Hudson <rob@cogit8.org>
2008-03-06 22:46:56 -0800

    Minor tweaks to models, views, urls.  Added templates directory with minimal base template.

--- ignite/presentation/models.py
+++ ignite/presentation/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Presentation(models.Model):
     """
@@ -6,9 +7,9 @@ class Presentation(models.Model):
     """
     presenter_name = models.CharField(maxlength=255, help_text="Name of person presenting.")
     presenter_email = models.EmailField(help_text="E-mail of person presenting for contact purposes, not displayed publicly.")
-    presenter_url = models.URLField(help_text="Optional URL to link presenter name to.")
-    presenter_bio = models.TextField(blank=True, help_text="Option short bio about presenter.")
+    presenter_url = models.URLField(blank=True, help_text="Optional URL to link presenter name to.")
+    presenter_bio = models.TextField(blank=True, help_text="Optional short bio about presenter.")
     title = models.CharField(maxlength=255, help_text="Title of presentation.")
     description = models.TextField(help_text="Full description of presentation.")
-    created = models.DateTimeField()
+    created = models.DateTimeField(default=datetime.now)
     # fields to help determine whether this presentation is chosen?
--- ignite/urls.py
+++ ignite/urls.py
@@ -1,9 +1,5 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^ignite/', include('ignite.foo.urls')),
-
-    # Uncomment this for admin:
-#     (r'^admin/', include('django.contrib.admin.urls')),
+     (r'^admin/', include('django.contrib.admin.urls')),
 )
