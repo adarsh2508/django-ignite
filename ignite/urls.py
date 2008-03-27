059b510a1087668499119e98213d88b07ebef1a4
Rob Hudson <rob@cogit8.org>
2008-03-26 20:12:42 -0700

    updating urls

--- ignite/urls.py
+++ ignite/urls.py
@@ -1,5 +1,20 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+from ignite.settings import PROJECT_PATH, DEBUG
 
 urlpatterns = patterns('',
-     (r'^admin/', include('django.contrib.admin.urls')),
+
+    # Home page
+    (r'^$', direct_to_template, {'template': 'home.html'}),
+
+    # Presentations
+    (r'^presentation/', include('ignite.presentation.urls')),
+
+    # Admin
+    (r'^admin/', include('django.contrib.admin.urls')),
 )
+
+if DEBUG:
+    urlpatterns += patterns('', 
+        (r'^media/(.*)$', 'django.views.static.serve', {'document_root': '%s/../media' % (PROJECT_PATH)})
+    )
