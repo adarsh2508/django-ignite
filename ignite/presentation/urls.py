3e8c256a133b8e7846d450802a76c25fa3843bbe
Rob Hudson <rob@cogit8.org>
2008-03-26 20:13:26 -0700

    adding presentation urls file

---
+++ ignite/presentation/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls.defaults import *
+from ignite.presentation.views import presentation_add, presentation_list, presentation_detail
+
+urlpatterns = patterns('',
+    url(r'^$', presentation_list, name='presentation_list'),
+    url(r'^add/$', presentation_add, name='presentation_add'),
+    url(r'^(?P<presentation_id>\d+)/$', presentation_detail, name='presentation_detail'),
+)
